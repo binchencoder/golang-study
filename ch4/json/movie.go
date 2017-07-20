bb9e9dccdee028f3ee6afca1d9471f3cd584485d
chenbin1314 <c1325791024@163.com>
2017-07-20 23:01:19 +0800

    movie.go

--- ch4/json/movie.go
+++ ch4/json/movie.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
 type Movie struct {
 	Title  string
@@ -12,6 +16,19 @@ type Movie struct {
 func main() {
 	var movies = []Movie{
 		{Title: "Casablanca", Year: 1942, Color: false, Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+		{Title: "Cool Hand Luke", Year: 1967, Color: true, Actors: []string{"Paul Newman"}},
 	}
 	fmt.Printf("%#v\n", movies)
+
+	data, err := json.Marshal(movies)
+	if err != nil {
+		log.Fatal("JSON marshaling failed: %s", err)
+	}
+	fmt.Printf("%s\n", data)
+
+	data1, err1 := json.MarshalIndent(movies, "", "  ")
+	if err1 != nil {
+		log.Fatal("JSON marshaling failed: %s", err1)
+	}
+	fmt.Printf("%s\n", data1)
 }
